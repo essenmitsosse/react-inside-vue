f21d80f445b9b471978cb34e2a7a9a9913f3c92f
Marcus Blättermann <marcus@essenmitsosse.de>
2023-04-04 13:10:54 +0200

    Improve styling

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,25 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="row">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </div>
+      <h1>Vite + Vue</h1>
+      <button type="button" @click="count++">
+        Vue Button: count is {{ count }}
+      </button>
+    </div>
+    <div class="row custom-react">
+      <custom-react :value="count" @setValue="setCount" />
+    </div>
   </div>
-  <h1>Vite + Vue</h1>
-  <button type="button" @click="count++">
-    Vue Button: count is {{ count }}
-  </button>
-  <custom-react :value="count" @setValue="setCount" />
 </template>
 
 <style scoped>
@@ -43,5 +49,19 @@ onMounted(() => {
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.container {
+  display: flex;
+}
+
+.custom-react {
+  outline: 2px red solid;
+  background: rgba(255, 0, 0, 0.1);
+}
+
+.custom-react:before {
+  content: "This is a React component wrapped in a Web component";
+  background: red;
 }
 </style>
