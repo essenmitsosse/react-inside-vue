2383b832d574ae2fd4176bff5cb830ba2651aa2c
Marcus Blättermann <marcus@essenmitsosse.de>
2023-04-04 13:12:58 +0200

    Remove global App style for React component

--- reactComponent/src/main.jsx
+++ reactComponent/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import style from "./index.css";
-import styleApp from "./App.css";
 
 class CustomReact extends HTMLElement {
   constructor() {
@@ -17,11 +16,8 @@ class CustomReact extends HTMLElement {
     const docStyle = document.createElement("style");
     docStyle.textContent = style;
 
-    const docStyle2 = document.createElement("style");
-    docStyle2.textContent = styleApp;
-
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.append(docStyle, docStyle2, this.mountPoint);
+    this.shadowRoot.append(docStyle, this.mountPoint);
   }
 
   static get observedAttributes() {
